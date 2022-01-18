e284109829dfa8de5ab9b1d3deb0634b697c3f6b
JJTech <jjtech@jjtech.dev>
2022-01-18 14:44:32 -0500

    Add files from old bot

---
+++ CompetitionAutonomous.java
@@ -0,0 +1,253 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.util.Log;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.teamcode.Shared.Drive;
+//import org.firstinspires.ftc.teamcode.Drive;
+
+import java.util.List;
+
+@Autonomous(name = "Competition", group = "Autonomous")
+//@Disabled
+public class CompetitionAutonomous extends LinearOpMode {
+    private final String TAG = getClass().getName();
+    private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
+    private static final String LABEL_FIRST_ELEMENT = "Quad";
+    private static final String LABEL_SECOND_ELEMENT = "Single";
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private Servo Load;
+    private DcMotorEx Ring;
+
+    private RevBlinkinLedDriver blinkinLedDriver;
+
+
+    private static final String VUFORIA_KEY =
+            "AUmiib//////AAABmbrFAeBqbkd/hmTwBoU6jXFUjeYK8xAgeYu6r9ZuLmpgb4tqNl4oIhXkpbBXmCusnhPlxJ3DHEkExTnQKhvCU49Yu2jslI6vaQ+V5F21ZAbbBod6lm9zyBEpkujo7IOq2TdOaJSIdN5wW3zxrHTksfrzBuKZKRsArompruh7jrm/B4W3F/EunA8ymkVoi29W84q81XMwJyonWlS2sd3pebXvLW0YOKmA63QgdmtSpp9XVAccwiH8ND8rk7FXlIIucim1Ig5FmVPLIx88t7doptXh8uiXfHHMqXc1T1MrRvfemYaUqyg7I5lYLNjLhuRmBZO3BM/qoyjPhpMVtGNh6+z3VgaKhP7O6zI07W0mmMfO";
+
+
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
+
+    @Override
+    public void runOpMode() {
+        Load = hardwareMap.get(Servo.class, "Load");
+        Ring = hardwareMap.get(DcMotorEx.class, "Ring");
+        blinkinLedDriver = hardwareMap.get(RevBlinkinLedDriver.class, "LEDs");
+
+        Ring.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.COLOR_WAVES_RAINBOW_PALETTE);
+
+        initVuforia();
+        initTfod();
+
+        if (tfod != null) {
+            tfod.activate();
+
+            // The TensorFlow software will scale the input images from the camera to a lower resolution.
+            // This can result in lower detection accuracy at longer distances (> 55cm or 22").
+            // If your target is at distance greater than 50 cm (20") you can adjust the magnification value
+            // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
+            // should be set to the value of the images used to create the TensorFlow Object Detection model
+            // (typically 1.78 or 16/9).
+
+            // Uncomment the following line if you want to adjust the magnification and/or the aspect ratio of the input images.
+            tfod.setZoom(1.75, 1.78);
+        }
+
+        Log.i(TAG, "runOpmode: Waiting for IMU...");
+
+        Drive drive = new Drive(this);
+        drive.init();
+
+        telemetry.addLine("IMU ready");
+        telemetry.update();
+        Log.i(TAG, "runOpmode: IMU ready");
+
+        waitForStart();
+
+        drive.turnSweeper(.3, 1);
+
+        runtime.reset();
+        int singleVotes = 0;
+        int quadVotes = 0;
+        int votes = 0;
+        long startMillis = System.currentTimeMillis();
+
+        while (System.currentTimeMillis() - startMillis < 2000) {
+            if (tfod != null) {
+                // getUpdatedRecognitions() will return null if no new information is available since
+                // the last time that call was made.
+                List<Recognition> updatedRecognitions = tfod.getRecognitions();
+                if (updatedRecognitions != null) {
+                    votes++;
+                    // step through the list of recognitions and display boundary info.
+                    int i = 0;
+                    for (Recognition recognition : updatedRecognitions) {
+                        if (recognition.getConfidence() < 0.3) {
+                            continue;
+                        }
+                        if (recognition.getLabel() == LABEL_FIRST_ELEMENT) {
+                            quadVotes++;
+                        } else {
+                            singleVotes++;
+                        }
+                    }
+                }
+            }
+            sleep(100);
+        }
+
+        telemetry.addData("votes (single, quad)", "%d, %d", singleVotes, quadVotes);
+        telemetry.addData("total votes:", votes);
+        telemetry.addData("vote ratios (single, quad)", "%.3f, %.3f", singleVotes/(double)votes, quadVotes/(double)votes);
+        telemetry.addLine("Waiting for IMU...");
+        telemetry.update();
+        Log.i(TAG, String.format("runOpMode: votes (single, quad) %d, %d", singleVotes, quadVotes));
+        Log.i(TAG, String.format("total votes", votes));
+        Log.i(TAG, String.format("vote ratios (single, quad)", "%.3f, %.3f", singleVotes/(double)votes, quadVotes/(double)votes));
+
+        telemetry.addData("votes (single, quad)", "%d, %d", singleVotes, quadVotes);
+        telemetry.addData("total votes:", votes);
+        telemetry.addData("vote ratios (single, quad)", "%.3f, %.3f", singleVotes/(double)votes, quadVotes/(double)votes);
+
+
+        drive.runUsingEncoder();
+
+        Log.i("DriveByEncoderOpMode", "************************ xyzabc *****************************");
+
+        double SPEED = 1;
+        if(singleVotes/(double)votes > 0.3){
+            //drive to square b
+            blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE);
+            drive.vroomVroomMonitorTicks(SPEED, 16, 18, 10);
+            Log.i("DriveByEncoderOpMode", "************************ xyzabc *****************************");
+            drive.vroomVroomMonitorTicks(SPEED/2, 0, 20, 10);
+            Log.i("DriveByEncoderOpMode", "************************ xyzabc *****************************");
+            drive.vroomVroomMonitorTicks(SPEED/2, -34, 34, 10);
+            drive.ceaseMotion();
+            sleep(150);
+            drive.turnSweeper(.7, 1.0);
+            sleep(1000);
+            Log.i("DriveByEncoderOpMode", "************************ xyzabc *****************************");
+            drive.vroomVroomMonitorTicks(SPEED/2, 10, -25, 10);
+            //sleep(1000);  //Need to keep sweeper still until wobble goal comes to rest. Remove this when there is more code below
+        }else if(quadVotes/(double)votes > 0.3){
+            //drive to square c
+            blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.RED);
+            drive.vroomVroomMonitorTicks(SPEED, 16, 12, 10);
+            Log.i("DriveByEncoderOpMode", "************************ xyzabc *****************************");
+            drive.vroomVroomMonitorTicks(SPEED/2, 0, 83, 10);
+            drive.ceaseMotion();
+            sleep(150);
+            drive.turnSweeper(.7, 1.0);
+            sleep(1000);  //Need to keep sweeper still until wobble goal comes to rest. Remove this when there is more code below
+            Log.i("DriveByEncoderOpMode", "************************ xyzabc *****************************");
+            drive.vroomVroomMonitorTicks(SPEED/2, -27, -46, 4);
+        }else{
+            //drive to square a
+            blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.GREEN);
+            drive.vroomVroomMonitorTicks(SPEED, 16, 16, 10);
+            Log.i("DriveByEncoderOpMode", "************************ xyzabc *****************************");
+            drive.vroomVroomMonitorTicks(SPEED/2, 0, 34, 10);
+            drive.ceaseMotion();
+            sleep(150);
+            drive.turnSweeper(.7, 1.0);
+            Log.i("DriveByEncoderOpMode", "************************ xyzabc *****************************");
+            drive.vroomVroomMonitorTicks(SPEED/2, -20, -10, 4);
+            // Log.i("DriveByEncoderOpMode", "************************ xyzabc *****************************");
+            // drive.vroomVroomMonitorTicks(SPEED/2, 0, -12, 4);
+            // Log.i("DriveByEncoderOpMode", "************************ xyzabc *****************************");
+            // drive.vroomVroomMonitorTicks(SPEED/2, 0, 12, 4);
+        }
+        drive.ceaseMotion();
+
+        Ring.setVelocity(270, AngleUnit.DEGREES);
+        //Ring.setPower(.95);
+        sleep(1000);
+
+        Load.setPosition(0.7);
+        for(int i = 0; i < 5; i++) {
+            blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP1_2_SPARKLE_2_ON_1);
+            sleep(100);
+            blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLACK);
+            sleep(100);
+        }
+        Load.setPosition(.05);
+        sleep(1000);
+
+        Load.setPosition(0.7);
+        for(int i = 0; i < 5; i++) {
+            blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP1_2_SPARKLE_2_ON_1);
+            sleep(100);
+            blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLACK);
+            sleep(100);
+        }
+        Load.setPosition(.05);
+        sleep(1000);
+
+        Load.setPosition(0.7);
+        for(int i = 0; i < 5; i++) {
+            blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP1_2_SPARKLE_2_ON_1);
+            sleep(100);
+            blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLACK);
+            sleep(100);
+        }
+        Load.setPosition(.05);
+        sleep(1000);
+
+        blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.COLOR_WAVES_RAINBOW_PALETTE);
+
+        drive.vroomVroomMonitorTicks(SPEED/2, 0, 16, 10);
+
+        drive.ceaseMotion();
+        sleep(500);
+
+        if (tfod != null) {
+            tfod.shutdown();
+        }
+    }
+
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = CameraDirection.BACK;
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
+    }
+
+    /**
+     * Initialize the TensorFlow Object Detection engine.
+     */
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.8f;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
+    }
+}
---
+++ CompetitionTeleOp2.java
@@ -0,0 +1,276 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.hardware.rev.RevTouchSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import org.firstinspires.ftc.teamcode.Shared.Drive;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+
+@TeleOp(name="Competition 2", group="TeleOp")
+//@Disabled
+public class CompetitionTeleOp2 extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    DcMotor leftFrontDrive, rightFrontDrive, leftBackDrive, rightBackDrive = null;
+    private Servo Load;
+    private DcMotorEx Ring, Sweep;
+    private DcMotor Elevate;
+    private RevTouchSensor HopperUpperLimit, HopperLowerLimit;
+    private RevBlinkinLedDriver blinkinLedDriver;
+
+    @Override
+    public void runOpMode() {
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "LM DT");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "RM DT");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "LR DT");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "RR DT");
+
+        rightFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        Load = hardwareMap.get(Servo.class, "Load");
+        Ring = hardwareMap.get(DcMotorEx.class, "Ring");
+        Elevate = hardwareMap.get(DcMotor.class, "Elevate");
+        Sweep = hardwareMap.get(DcMotorEx.class, "Sweep");
+
+        Ring.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Sweep.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        HopperUpperLimit = hardwareMap.get(RevTouchSensor.class, "HopperUpperLimitSensor");
+        HopperLowerLimit = hardwareMap.get(RevTouchSensor.class, "HopperLowerLimitSensor");
+
+        blinkinLedDriver = hardwareMap.get(RevBlinkinLedDriver.class, "LEDs");
+
+        final AtomicBoolean isSweeperThreadInUse = new AtomicBoolean();
+        isSweeperThreadInUse.set(false);
+
+//        // set the digital channel to input.
+//        HopperUpperLimit.setMode(DigitalChannel.Mode.INPUT);
+//        HopperLowerLimit.setMode(DigitalChannel.Mode.INPUT);
+
+        Drive drive = new Drive(this);
+        drive.init();
+
+        waitForStart();
+
+        Load.setPosition(0);
+
+        while (opModeIsActive()) {
+            // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
+            double frontRightPowerFactor, frontLeftPowerFactor, backRightPowerFactor, backLeftPowerFactor;
+            double magRight = Math.hypot(gamepad1.right_stick_x, gamepad1.right_stick_y);
+            double thetaRight = Math.atan2(-gamepad1.right_stick_y, gamepad1.right_stick_x);
+            double magLeft = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+            double thetaLeft = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x);
+            double pi = Math.PI;
+
+        /*if (thetaRight > 0 && thetaRight < pi / 2) {
+            frontRightPowerFactor = -Math.cos(2 * thetaRight);
+        } else if (thetaRight >= -pi && thetaRight < -pi / 2) {
+            frontRightPowerFactor = Math.cos(2 * thetaRight);
+        } else if (thetaRight >= pi / 2 && thetaRight <= pi) {
+            frontRightPowerFactor = 1;
+        } else {
+            frontRightPowerFactor = -1;
+        }
+        if (thetaLeft > 0 && thetaLeft < pi / 2) {
+            backLeftPowerFactor = -Math.cos(2 * thetaLeft);
+        } else if (thetaLeft >= -pi && thetaLeft < -pi / 2) {
+            backLeftPowerFactor = Math.cos(2 * thetaLeft);
+        } else if (thetaLeft >= pi / 2 && thetaLeft <= pi) {
+            backLeftPowerFactor = 1;
+        } else {
+            backLeftPowerFactor = -1;
+        }
+        if (thetaRight > -pi / 2 && thetaRight < 0) {
+            backRightPowerFactor = Math.cos(2 * thetaRight);
+        } else if (thetaRight > pi / 2 && thetaRight < pi) {
+            backRightPowerFactor = -Math.cos(2 * thetaRight);
+        } else if (thetaRight >= 0 && thetaRight <= pi / 2) {
+            backRightPowerFactor = 1;
+        } else {
+            backRightPowerFactor = -1;
+        }
+        if (thetaLeft > -pi / 2 && thetaLeft < 0) {
+            frontLeftPowerFactor = Math.cos(2 * thetaLeft);
+        } else if (thetaLeft > pi / 2 && thetaLeft < pi) {
+            frontLeftPowerFactor = -Math.cos(2 * thetaLeft);
+        } else if (thetaLeft >= 0 && thetaLeft <= pi / 2) {
+            frontLeftPowerFactor = 1;
+        } else {
+            frontLeftPowerFactor = -1;
+        }*/
+
+            if (thetaRight > 0 && thetaRight < pi / 2) {
+                frontRightPowerFactor = -Math.cos(2 * thetaRight);
+            } else if (thetaRight >= -pi && thetaRight < -pi / 2) {
+                frontRightPowerFactor = Math.cos(2 * thetaRight);
+            } else if (thetaRight >= pi / 2 && thetaRight <= pi) {
+                frontRightPowerFactor = 1;
+            } else {
+                frontRightPowerFactor = -1;
+            }
+
+            if (thetaLeft > 0 && thetaLeft < pi / 2) {
+                backLeftPowerFactor = -Math.cos(2 * thetaLeft);
+            } else if (thetaLeft >= -pi && thetaLeft < -pi / 2) {
+                backLeftPowerFactor = Math.cos(2 * thetaLeft);
+            } else if (thetaLeft >= pi / 2 && thetaLeft <= pi) {
+                backLeftPowerFactor = 1;
+            } else {
+                backLeftPowerFactor = -1;
+            }
+
+            if (thetaRight > -pi / 2 && thetaRight < 0) {
+                backRightPowerFactor = Math.cos(2 * thetaRight);
+            } else if (thetaRight > pi / 2 && thetaRight < pi) {
+                backRightPowerFactor = -Math.cos(2 * thetaRight);
+            } else if (thetaRight >= 0 && thetaRight <= pi / 2) {
+                backRightPowerFactor = 1;
+            } else {
+                backRightPowerFactor = -1;
+            }
+
+            if (thetaLeft > -pi / 2 && thetaLeft < 0) {
+                frontLeftPowerFactor = Math.cos(2 * thetaLeft);
+            } else if (thetaLeft > pi / 2 && thetaLeft < pi) {
+                frontLeftPowerFactor = -Math.cos(2 * thetaLeft);
+            } else if (thetaLeft >= 0 && thetaLeft <= pi / 2) {
+                frontLeftPowerFactor = 1;
+            } else {
+                frontLeftPowerFactor = -1;
+            }
+
+            // for test robot (just chassis & camera)
+//        if(gamepad1.a){
+//            leftFrontDrive.setPower(0.5);
+//            leftBackDrive.setPower(0.5);
+//            rightFrontDrive.setPower(0.5);
+//            rightBackDrive.setPower(0.5);
+//        }else if(gamepad1.b){
+//            leftFrontDrive.setPower(-0.5);
+//            leftBackDrive.setPower(-0.5);
+//            rightFrontDrive.setPower(-0.5);
+//            rightBackDrive.setPower(-0.5);
+//        }else{
+//            leftFrontDrive.setPower(frontLeftPowerFactor * magLeft);
+//            rightFrontDrive.setPower(frontRightPowerFactor * magRight);
+//            leftBackDrive.setPower(backLeftPowerFactor * magLeft);
+//            rightBackDrive.setPower(backRightPowerFactor * magRight);
+//        }
+
+            // for actual competition robot
+//        if(gamepad1.a){
+//            leftFrontDrive.setPower(-0.5);
+//            leftBackDrive.setPower(-0.5);
+//            rightFrontDrive.setPower(-0.5);
+//            rightBackDrive.setPower(-0.5);
+//        }else if(gamepad1.b){
+//            leftFrontDrive.setPower(0.5);
+//            leftBackDrive.setPower(0.5);
+//            rightFrontDrive.setPower(0.5);
+//            rightBackDrive.setPower(0.5);
+//        }else{
+            int sign = 1;
+            if(frontLeftPowerFactor >= 0) sign = 1; else sign = -1;
+            leftFrontDrive.setPower(sign*(frontLeftPowerFactor * magLeft)*(frontLeftPowerFactor * magLeft));
+            if(frontRightPowerFactor >= 0) sign = 1; else sign = -1;
+            rightFrontDrive.setPower(-sign*(frontRightPowerFactor * magRight)*(frontRightPowerFactor * magRight));
+            if(backLeftPowerFactor >= 0) sign = 1; else sign = -1;
+            leftBackDrive.setPower(sign*(backLeftPowerFactor * magLeft)*(backLeftPowerFactor * magLeft));
+            if(backRightPowerFactor >= 0) sign = 1; else sign = -1;
+            rightBackDrive.setPower(-sign*(backRightPowerFactor * magRight)*(backRightPowerFactor * magRight));
+//        }
+
+            double launcherSpeed;
+            if (gamepad2.right_trigger > 0.5) {
+                launcherSpeed = 275;
+            } else if (gamepad2.left_trigger > 0.5) {
+                launcherSpeed = 255;
+            } else {
+                launcherSpeed = 0;
+            }
+
+            Ring.setVelocity(launcherSpeed, AngleUnit.DEGREES);
+
+            if(HopperLowerLimit.isPressed()){
+                if(gamepad2.right_stick_y > 0){
+                    Elevate.setPower(0);
+                }else{
+                    Elevate.setPower(0.5 * gamepad2.right_stick_y);
+                }
+            }else if(HopperUpperLimit.isPressed()){
+                if(gamepad2.right_stick_y < 0){
+                    Elevate.setPower(0);
+                }else{
+                    Elevate.setPower(0.5 * gamepad2.right_stick_y);
+                }
+            }else{
+                Elevate.setPower(0.5 * gamepad2.right_stick_y);
+            }
+
+            telemetry.addData("runOpMode: ", "HopperLowerLimit State: %s | HopperUpperLimit State: %s", HopperLowerLimit.isPressed(), HopperUpperLimit.isPressed());
+            telemetry.addData("runOpMode: ", "gamepad right stick y: %.2f", gamepad2.right_stick_y);
+
+            if (gamepad2.x) {
+                Sweep.setVelocity(-280, AngleUnit.DEGREES);
+            }else if (gamepad2.y) {
+                Sweep.setVelocity(0, AngleUnit.DEGREES);
+            }else if (gamepad2.b) {
+                Sweep.setVelocity(280, AngleUnit.DEGREES);
+            }
+
+            Thread shooterThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Load.setPosition(0.70);
+                    //delay must add to 900ms
+                    for(int i = 0; i < 5; i++) {
+                        blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP1_2_SPARKLE_2_ON_1);
+                        sleep(90);
+                        blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLACK);
+                        sleep(90);
+                    }
+                    Load.setPosition(.05);
+                    //delay must add up to 1000ms
+                    blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.COLOR_WAVES_RAINBOW_PALETTE);
+                    sleep(1000);
+                    isSweeperThreadInUse.set(false);
+                }
+            });
+
+            if (gamepad2.a && HopperUpperLimit.isPressed() && isSweeperThreadInUse.compareAndSet(false, true)) {
+                shooterThread.start();
+            }
+
+            telemetry.addData("IMU Orientation: ", drive.getImuDeltaAngle());
+
+            telemetry.addData("front right power ", ((float) Math.round(rightFrontDrive.getPower() * 100)) / 100);
+            telemetry.addData("front left power ", ((float) Math.round(leftFrontDrive.getPower() * 100)) / 100);
+            telemetry.addData("back right power ", ((float) Math.round(rightBackDrive.getPower() * 100)) / 100);
+            telemetry.addData("back left power ", ((float) Math.round(leftBackDrive.getPower() * 100)) / 100);
+            telemetry.addData("left joystick x", ((float) Math.round(gamepad1.left_stick_x * 100)) / 100);
+            telemetry.addData("left joystick y", ((float) Math.round(-gamepad1.left_stick_y * 100)) / 100);
+            telemetry.addData("magnitude left", ((float) Math.round(magLeft * 100)) / 100);
+            telemetry.addData("thetaLeft", ((float) Math.round(thetaLeft / pi * 100)) / 100);
+
+            telemetry.update();
+        }
+        blinkinLedDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.COLOR_WAVES_RAINBOW_PALETTE);
+    }
+}
